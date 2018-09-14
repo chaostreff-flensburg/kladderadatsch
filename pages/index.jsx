f7994ea3025282a496e47e0c60b0a08f7cd26b29
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2018-09-14 15:15:36 +0200

    Added react-beautiful-dnd

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 class Page extends React.Component {
-
   render() {
     return (
       <React.Fragment>
         <h1>Hello Kladderadatsch!</h1>
+        <DragDropContext>
+          <Droppable droppableId="tasklist">
+            {(provided, snapshot) => (
+              <main ref={provided.innerRef} {...provided.droppableProps}>
+                <Draggable draggableId="draggable-1">
+                  {(provided, snapshot) => (
+                    <h4
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      My draggable
+                    </h4>
+                  )}
+                </Draggable>
+              </main>
+            )}
+          </Droppable>
+        </DragDropContext>
         <style jsx>{`
           h1 {
             font-family: sans-serif;
